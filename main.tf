b4aee35204fdf18fc0c3f502c4bc723136332337
nareshgandi <66723519+nareshgandi@users.noreply.github.com>
2024-01-15 12:51:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,22 +2,6 @@ provider "aws" {
     region = "us-east-1"  
 }
 
-# Replace "vpc-12345678" with the ID of your existing VPC
-data "aws_vpc" "existing_vpc" {
-  id = "vpc-0f985705714cc8ea7"
-}
-
-
-# Replace "sg-12345678" with the ID of your existing security group
-data "aws_security_group" "existing_security_group" {
-  id = "sg-0c74eb2fad61f0001"
-}
-
-# Replace "subnet-group-name" with the name of your existing DB subnet group that includes the chosen subnet(s) for the RDS instance
-data "aws_db_subnet_group" "existing_db_subnet_group" {
-  name = "rdsa-postgresql-db-subnet-group"
-}
-
 # Create the PostgreSQL RDS instance
 resource "aws_db_instance" "postgres" {
   identifier             = "my-postgres-db"
@@ -33,8 +17,8 @@ resource "aws_db_instance" "postgres" {
   skip_final_snapshot    = true  # Set to false if you want to create a final snapshot on deletion
 
   # Use the existing VPC, subnet, and subnet group IDs/names
-  vpc_security_group_ids = [data.aws_security_group.existing_security_group.id]
-  db_subnet_group_name    = data.aws_db_subnet_group.existing_db_subnet_group.name
+  vpc_security_group_ids = "sg-0c74eb2fad61f0001"
+  db_subnet_group_name   = "rdsa-postgresql-db-subnet-group"
 
   # You can set the "availability_zone" attribute if you want to specify a specific AZ for the RDS instance
   # availability_zone = "us-east-1a"
